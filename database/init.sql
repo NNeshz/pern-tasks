b375a18f2c0794d4d6e807dd3f44d283ff68f815
Nefta Hernandez <neftaligow01@gmail.com>
2023-09-19 20:57:40 -0600

    Ruta de signup terminada

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,14 @@
-CREATE TABLE IF NOT EXISTS `tasks` (
-    `id` SERIAL PRIMARY KEY,
-    `title` VARCHAR(255) UNIQUE NOT NULL,
-    `description` TEXT,
-)
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT,
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,9 +1,34 @@
+import { pool } from "../db.js";
+import bcrypt from "bcrypt";
+
+import { createAccesToken } from "../libs/jwt.js"
+
 export const signin = (req, res) => {
   res.send("signin");
 };
 
-export const signup = (req, res) => {
-  res.send("signup");
+export const signup = async (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      "INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *",
+      [email, hashedPassword, name]
+    );
+
+    const token = await createAccesToken({ id: result.rows[0].id})
+    // res.json(result.rows[0]);
+    return res.json({
+      token: token
+    })
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(409).json({ message: "Usuario ya existe" });
+    }
+    next(error);
+  }
 };
 
 export const signout = (req, res) => {
