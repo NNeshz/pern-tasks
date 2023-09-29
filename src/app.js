fe2acc4d7c71555fa78f67f01ca66e718a9a275d
Nefta Hernandez <neftaligow01@gmail.com>
2023-09-29 14:00:50 -0600

    Production ready

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to my new API");
 });
 
-app.get("api/ping", async (req, res) => {
+app.get("/api/ping", async (req, res) => {
   const response = await pool.query("SELECT NOW()")
   res.send(response.rows[0]);
 })
